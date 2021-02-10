4edf99fc58689b1a7f554262773d91c95895ffab
Lwasampijja Baker <bubakes2000@gmail.com>
2021-02-10 23:22:25 +0300

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,7 +5,7 @@ import requests
 import pandas as pd
 import warnings
 
-#Ignore all warnings shown here
+#Ignore all pyhon warnings
 from pandas import json_normalize
 
 warnings.filterwarnings('ignore')
